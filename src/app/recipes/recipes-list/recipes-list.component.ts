931088ffcb6e1d29e44e8050207233ee057372e6
jennybk15 <jennyb@hotelbeds.com>
2020-01-26 17:05:43 +0200

    added recipe details and recipe item content

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,15 +7,29 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipes-list.component.css']
 })
 export class RecipesListComponent implements OnInit {
+  @Input() recipe: Recipe;
+  @Output() recipeSelected = new EventEmitter<Recipe>();
+
   recipes: Recipe[] = [
-    new Recipe('Test recipe 1', 'test blabla', 'https://mommyshomecooking.com/wp-content/uploads/2018/03/Easy-Whole-30-Chicken-and-Asparagus-Skillet-1-768x1075.jpg'),
-    new Recipe('Test recipe 2', 'test blu blu', 'https://www.fifteenspatulas.com/wp-content/uploads/2015/12/Oreo-Truffles-Easy-Dessert-Recipes-Fifteen-Spatulas-2-640x960.jpg'),
-    new Recipe('Test recipe 3', 'test aaaaaa', 'https://static.parade.com/wp-content/uploads/2019/10/Mini-Cherry-Cheesecakes-Recipe.jpg')
+    new Recipe('Test recipe 1',
+      'test blabla',
+      'https://mommyshomecooking.com/wp-content/uploads/2018/03/Easy-Whole-30-Chicken-and-Asparagus-Skillet-1-768x1075.jpg'),
+    new Recipe('Test recipe 2',
+      'test blu blu',
+      'https://www.fifteenspatulas.com/wp-content/uploads/2015/12/Oreo-Truffles-Easy-Dessert-Recipes-Fifteen-Spatulas-2-640x960.jpg'),
+    new Recipe('Test recipe 3',
+      'test aaaaaa',
+      'https://static.parade.com/wp-content/uploads/2019/10/Mini-Cherry-Cheesecakes-Recipe.jpg')
   ];
 
   constructor() { }
 
   ngOnInit() {
+
+  }
+
+  onRecipeSelected(selectedRecipe: Recipe) {
+    this.recipeSelected.emit(selectedRecipe);
   }
 
 }
